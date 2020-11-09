2ae0d2ab9882429a49e36c89e1e6bb1abe747a73
ivonice <ivoniceoliveira.coroa@gmail.com>
2020-11-09 16:28:57 -0300

    adicionando o arquivo bondinho

---
+++ bondinho.py
@@ -0,0 +1,8 @@
+print("Olá esse programa tem como objetivo informa se será possivel embarca a quantidade de pessos no bondinho, ONDE S é sim e N é não")
+
+a= int(input("Qual o numero de alunos"))
+n= int(input("Qual o numero de monitores "))
+if a + n <=50:
+    print("S")
+else:
+    print("N")
